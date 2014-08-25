138cd77376ea3712b96f59a2a4571a4cf54bf8d8
Ryan Anderson <ryan.anderson.01@gmail.com>
2014-08-24 23:01:40 -0400

    merged

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -2,16 +2,27 @@
 
 '''Primary script for testing'''
 
-from unlabeled import *
+from orderly import *
+import sys, argparse
 
+def setupArgs():
+    '''Set up command line arguments'''
+    parser = argparse.ArgumentParser(description='Program doe enumerating unlabeled graphs')
 
-def main():
-    edges = 4
-    g = unlabeledCompliment(4)
+    parser.add_argument('--vertices', '-n', default=4,
+                        type=int, help='Max number of vetices', dest='vertices',
+                        metavar='<max vertices>')
+    return parser
+
+def main(argv):
+    args = setupArgs().parse_args()
+    vertices = args.vertices
+
+    g = unlabeledCompliment(vertices)
 
     for i in g:
         print i
 
 if __name__ == "__main__":
-	main()
+	main(sys.argv)
 
